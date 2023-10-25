68aaa8ef37a4ab443e6356e2462af6f938c6f500
Murial Anindo <murial.anindo@lonett.com>
2023-10-25 15:24:22 -0400

    adding the api-routes

--- server.js
+++ server.js
@@ -6,6 +6,22 @@ app.get("/", function (req, res) {
   res.send("It's working!");
 });
 
+//endpoint to get Many
+app.get("/locations", function (res, req) {
+  res.send();
+});
+//Add server endpoint to GET a single
+app.get("/locations:id", function (req, res) {
+  res.send();
+});
+
+//Create menthod
+app.put("/locations:id", function (req, res) {});
+//Update menthod
+app.post("/locations:id", function (req, res) {});
+//delete method
+app.delete("/location:id", function (req, res) {});
+
 app.listen(port, () => {
   console.log("app listening on port 5000");
 });
