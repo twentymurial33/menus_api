69a9b522dd3bdfe0a3323b57cdc8fb932f22406c
Murial Anindo <murial.anindo@lonett.com>
2023-10-25 12:40:04 -0400

    trying to see what is wrong with server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const app = express();
+const port = 5000;
 
 app.get("/", function (req, res) {
   res.send("It's working!");
 });
 
-app.listen(3000, () => {
-  console.log("app listening on port 3000");
+app.listen(port, () => {
+  console.log("app listening on port 5000");
 });
